1660ad14c2d85b23262b8da55b350353bc44bfdb
tinnit1 <juanpalacor@gmail.com>
2023-08-14 12:06:31 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,12 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
+</p>
+
+# Development environment
+
+1. clone the project
+2. rename `env.template` instead `.env`
+3. execute `yarn install`
+4. run local database `docker-compose up -d`
+5. start the project `yarn start:dev`
+6. access to `localhost:3000/graphql`
---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { join } from 'path';
+import { ItemsModule } from './items/items.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      // debug: false,
+      playground: false,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+    ItemsModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
